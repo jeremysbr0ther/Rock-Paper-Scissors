c3cef6dc9e2bacffa1fd58ac91eb20c98bb6ffff
Alp Oborcu <jeremysbrother.jb@gmail.com>
2022-09-18 13:25:57 +0300

    One round game is done

--- script.js
+++ script.js
@@ -4,7 +4,11 @@
 // Then create a loop for 5 rounds
 // Break if someone get 3 
 
-const rockPaperScissor = ['Rock', 'Paper', 'Scissor'];
+let playerSelection = prompt('Plese select one of the following; Rock-Paper-Scissor: ');
+playerSelection = playerSelection.toLowerCase();
+console.log(playerSelection);
+
+const rockPaperScissor = ['rock', 'paper', 'scissor'];
 function getComputerChoise(){
     let choiceNumber = Math.floor(Math.random() * 3);
     computerChoice = rockPaperScissor[choiceNumber];
@@ -12,3 +16,36 @@ function getComputerChoise(){
 
 getComputerChoise();
 console.log(computerChoice);
+
+function playRound(playerSelection, computerChoice){
+    if (playerSelection === computerChoice) {
+        result = 'Lets call it draw.';
+        return result;
+    }
+    else if (playerSelection === 'rock' && computerChoice === 'paper') {
+        result = 'You lose! Paper beats rock.';
+        return result;
+    }
+    else if (playerSelection === 'rock' && computerChoice === 'scissor') {
+        result = 'You win! Rock beats scissor.';
+        return result;
+    }
+    else if (playerSelection === 'paper' && computerChoice === 'scissor') {
+        result = 'You lose! Scissor beats paper.';
+        return result;
+    }
+    else if (playerSelection === 'paper' && computerChoice === 'rock') {
+        result = 'You win! Paper beats rock.';
+        return result;
+    }
+    else if (playerSelection === 'scissor' && computerChoice === 'rock') {
+        result = 'You lose! Rock beats scissor.';
+        return result;
+    }
+    else if (playerSelection === 'scissor' && computerChoice === 'paper') {
+        result = 'You win! Scissor beats paper.';
+        return result;
+    }
+}
+
+console.log(playRound(playerSelection, computerChoice));
